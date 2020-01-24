72cf28e673cf57b0eb144131eb12ff02e04a3659
Galvus Damor <gregor.behnke@uni-ulm.de>
2020-01-24 14:10:04 +0100

    coloring of the output can be turned off, allowed for specifying background colors

--- src/util.hpp
+++ src/util.hpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-enum Color{COLOR_GRAY,COLOR_RED,COLOR_GREEN,COLOR_YELLOW,COLOR_BLUE,COLOR_PURPLE,COLOR_CYAN};
-enum Mode{MODE_NORMAL,MODE_BOLD,MODE_X,MODE_Y,MODE_Z,MODE_UNDERLINE};
+enum Color{COLOR_GRAY,COLOR_RED,COLOR_GREEN,COLOR_YELLOW,COLOR_BLUE,COLOR_PURPLE,COLOR_CYAN,COLOR_WHITE,COLOR_NONE};
+enum Mode{MODE_NORMAL,MODE_BOLD,MODE_CURSIVE,MODE_Y,MODE_UNDERLINE,MODE_BLINK};
 
-string color (Color color, string text, Mode m = MODE_NORMAL);
+string color (Color color, string text, Mode m = MODE_NORMAL, Color background = COLOR_NONE);
+
+extern bool no_colors_in_output;
