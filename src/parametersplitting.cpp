ff5a3ffa6aeaf4c8f36560722b0347516fd6d5d2
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-10-14 12:24:52 +0200

    Bugfixed Parametersplitting

--- src/parametersplitting.cpp
+++ src/parametersplitting.cpp
@@ -16,10 +16,11 @@ void split_independent_parameters(){
 	}
 
 
-	//cout << "Static predicates are: " << endl;
-	//for (string s : staticPredicates)
-	//	cout << s << endl;
-	
+#ifndef NDEBUG
+	cout << "Static predicates are: " << endl;
+	for (string s : staticPredicates)
+		cout << s << endl;
+#endif	
 
 	bool splittedSomeMethod = true;
 	int i = 0;
@@ -54,11 +55,29 @@ void split_independent_parameters(){
 						pCount++;
 					}
 					
+					// handle constraints as if they were static condition (they are ...)
+					for (literal & l : task_name_map[ps.task].constraints){
+						for (string & a : l.arguments)
+							variables_ps_id[a].insert({ps.id,pCount});
+						pCount++;
+					}
+	
 					for(string v : vars_in_non_static)
 						variables_ps_id[v].insert({ps.id,-1});
 				}
 			}
-	
+
+#ifndef NDEBUG
+			cout << "====================" << endl << "Method: " << m.name << endl;
+			for (auto ps : m.ps)
+				cout << "\t\tPS: " << ps.id << " " << ps.task << endl;
+			for (auto [v,ss] : variables_ps_id){
+				cout << "\tvar: " << v << endl;
+				for (auto [ps,prec] : ss)
+					cout << "\t\t" << ps << " @ " << prec << endl;
+			}	
+#endif	
+		
 			map<string,string> variableSorts;
 			for(auto v : m.vars) variableSorts[v.first] = v.second;
 			// don't consider variables that have at most one instantiation
@@ -71,20 +90,7 @@ void split_independent_parameters(){
 			// no variables can be splitted
 			if (variables_ps_id.size() == 0) { methods.push_back(m); continue; }
 	
-			/*cout << "====================" << endl << "Method: " << m.name << endl;
-			for (auto ps : m.ps)
-				cout << "\t\tPS: " << ps.id << " " << ps.task << endl;
-			for (auto [v,ss] : variables_ps_id){
-				cout << "\tvar: " << v << endl;
-				for (auto [ps,prec] : ss)
-					cout << "\t\t" << ps << " @ " << prec << endl;
-			}*/	
-			
-			/*set<string> allElems;
-			for (auto [vps,ss] : variables_ps_id) 
-				for (auto [ps,prec] : ss) allElems.insert(ps);
-*/
-	
+
 			string largestSplittable = "";
 			// check for every splittable variable whether we can split it away.
 			set<string> beforeID, afterID;
@@ -100,8 +106,10 @@ void split_independent_parameters(){
 // splitting on everything does not bring anything ...
 				if (all_ps.size() == m.ps.size()) continue; 
 				
-				//cout << "Var: " << vps << endl;
-				//for (string ps : ordered_ps) cout << "\t" << ps << endl; 
+#ifndef NDEBUG
+				cout << "Var: " << vps << endl;
+				for (string ps : ordered_ps) cout << "\t" << ps << endl; 
+#endif	
 	
 				set<string> newbeforeID, newafterID;
 				if (m.is_sub_group(ordered_ps, newbeforeID, newafterID)){
@@ -116,9 +124,11 @@ void split_independent_parameters(){
 
 			splittedSomeMethod = true;
 
-			//cout << "Largest Splittable: " << largestSplittable << endl;
-			// gather all variables that occur only in this group (might be more just this one)
+#ifndef NDEBUG
+			cout << "Largest Splittable: " << largestSplittable << endl;
+#endif
 	
+			//gather all variables that occur only in this group (might be more just this one)
 			set<pair<string,int>> subgroup = variables_ps_id[largestSplittable];
 			set<string> subgroupPSIDs;
 			for (auto [id,_x] : subgroup) subgroupPSIDs.insert(id);
@@ -133,10 +143,12 @@ void split_independent_parameters(){
 					subgroup.insert(occ.begin(), occ.end());
 				}
 			}
-			//cout << "Variables in splitting subgroup:";
-			//for (string v : variables_in_sub_group) cout << " " << v;
-			//cout << endl;
-	
+#ifndef NDEBUG
+			cout << "Variables in splitting subgroup:";
+			for (string v : variables_in_sub_group) cout << " " << v;
+			cout << endl;
+#endif
+
 			map<string,string> variables_to_remove;
 			for (auto v : variables_in_sub_group)
 				variables_to_remove[v].size(); // insert into map
@@ -155,16 +167,7 @@ void split_independent_parameters(){
 			sm.name = "_splitting_method_" + at.name; // must start with an underscore s.t. this method will be removed by the solution compiler
 			sm.at = at.name;
 			
-			// ordering
-			base.ordering.clear();
-			for (auto [b,a] : m.ordering){
-				int c = 0;
-				if (subgroup.count({b,-1})) c++;
-				if (subgroup.count({a,-1})) c++;
-				if (c == 0)	base.ordering.push_back({b,a});
-				if (c == 2)	sm.ordering.push_back({b,a});
-			}
-			//base.ordering = m.ordering;
+			
 			// only take variables that are not to be removed
 			map<string,string> sorts_of_remaining;
 			for(auto v : m.vars)
@@ -175,16 +178,18 @@ void split_independent_parameters(){
 		
 			set<string> smVars; // for duplicate check
 			
-			set<string> subSplittedIDs;
+			set<string> splittedIDs;
 			// put the plan steps in their respective methods
 			for (plan_step ops : m.ps){
 				// check if this an artificial task which we may have to split
 				set<int> splitPrecs;
+				set<string> haveMinusOne;
 				bool isSub = false;
 				for (auto [id,prec] : subgroup){
 					if (id != ops.id) continue;
 					if (prec == -1){
 						isSub = true;
+						haveMinusOne.insert(id);
 					} else {
 						splitPrecs.insert(prec);
 					}
@@ -200,16 +205,21 @@ void split_independent_parameters(){
 					task tSub;  plan_step pSub;
 					pBase.id = ops.id;
 					pSub.id = ops.id;
-					subSplittedIDs.insert(pSub.id);
+					splittedIDs.insert(pSub.id);
 					tBase.artificial = true;
 					tSub.artificial = true;
-	
+
+
+					if (haveMinusOne.count(pBase.id)){
+						beforeID.insert(pBase.id); 
+					}
+
 					// create names
 					tBase.name = tMain.name + "_base"; pBase.task = tBase.name;
 					tSub.name = tMain.name + "_split"; pSub.task = tSub.name;
 	
 					// contains only preconditions ...
-					for (size_t i = 0; i < tMain.prec.size(); i++)
+					for (size_t i = 0; i < tMain.prec.size(); i++){
 						if (splitPrecs.count(i))
 							tSub.prec.push_back(tMain.prec[i]);
 						else {
@@ -222,10 +232,28 @@ void split_independent_parameters(){
 							else
 								tBase.prec.push_back(tMain.prec[i]);
 						}
-	
+					}
+					// an constraints
+					for (size_t i = 0; i < tMain.constraints.size(); i++){
+						if (splitPrecs.count(i + tMain.prec.size()))
+							tSub.constraints.push_back(tMain.constraints[i]);
+						else {
+							bool needsSplit = false;
+							for (string arg : tMain.constraints[i].arguments)
+								needsSplit |= variables_in_sub_group.count(arg);
+
+							if (needsSplit)
+								tSub.constraints.push_back(tMain.constraints[i]);
+							else
+								tBase.constraints.push_back(tMain.constraints[i]);
+						}
+					}
+
 					set<string> varsSub, varsBase;
 					for (literal l : tSub.prec) for (string v : l.arguments) varsSub.insert(v);
 					for (literal l : tBase.prec) for (string v : l.arguments) varsBase.insert(v);
+					for (literal l : tSub.constraints) for (string v : l.arguments) varsSub.insert(v);
+					for (literal l : tBase.constraints) for (string v : l.arguments) varsBase.insert(v);
 	
 					// create variables
 					for (size_t vnum = 0; vnum < tMain.vars.size(); vnum++){
@@ -240,8 +268,11 @@ void split_independent_parameters(){
 						}
 					}
 					tBase.number_of_original_vars = tBase.vars.size();
-					tMain.number_of_original_vars = tMain.vars.size();
+					tSub.number_of_original_vars = tSub.vars.size();
 	
+					tBase.check_integrity();
+					tSub.check_integrity();
+
 					primitive_tasks.push_back(tBase); task_name_map[tBase.name] = tBase;
 					primitive_tasks.push_back(tSub);  task_name_map[tSub.name] = tSub;
 					
@@ -249,10 +280,20 @@ void split_independent_parameters(){
 					sm.ps.push_back(pSub);
 				}
 			}
+
+			// ordering
+			base.ordering.clear();
+			for (auto [b,a] : m.ordering){
+				int c = 0;
+				if (subgroup.count({b,-1}) && !splittedIDs.count(b)) c++;
+				if (subgroup.count({a,-1}) && !splittedIDs.count(b)) c++;
+				if (c == 0)	base.ordering.push_back({b,a});
+				if (c == 2)	sm.ordering.push_back({b,a});
+			}
 	
 			for (plan_step ps : sm.ps){
-				if (subSplittedIDs.count(ps.id)) continue;
-				for (string id : subSplittedIDs)
+				if (splittedIDs.count(ps.id)) continue;
+				for (string id : splittedIDs)
 					sm.ordering.push_back({id,ps.id});
 			}
 	
@@ -283,6 +324,7 @@ void split_independent_parameters(){
 	
 			// the new abstract task is done so push it
 			at.number_of_original_vars = at.vars.size(); // does not matter as it will get pruned
+			at.check_integrity();
 			abstract_tasks.push_back(at);
 			task_name_map[at.name] = at;
 	
@@ -295,11 +337,15 @@ void split_independent_parameters(){
 			base.ps.push_back(nps);
 	
 			sm.atargs = nps.args;
-			//cout << "SM" << endl;
+#ifndef NDEBUG
+			cout << "Checking splitting method" << endl;
+#endif
 			sm.check_integrity();
 			methods.push_back(sm);
 	
-			//cout << "BASE" << endl;
+#ifndef NDEBUG
+			cout << "Checking base method" << endl;
+#endif
 			base.check_integrity();
 			methods.push_back(base);
 		}
