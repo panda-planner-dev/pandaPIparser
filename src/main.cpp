27852c2afba7db39492510bf313d7e5010e7a37c
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2021-05-04 17:54:54 +0200

    Switched commandline to gengetopt

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,8 @@
 #include "verify.hpp"
 #include "properties.hpp"
 
+#include "cmdline.h"
+
 using namespace std;
 
 // declare parser function manually
@@ -76,86 +78,97 @@ int main(int argc, char** argv) {
 	bool showProperties = false;
 	int verbosity = 0;
 	
-	struct option options[] = {
-		{"no-split-parameters"                    , no_argument,       NULL,   's'},
-		{"keep-conditional-effects"               , no_argument,       NULL,   'k'},
-		{"linear-conditional-effect"              , no_argument,       NULL,   'L'},
-		{"encode-disjunctive_preconditions-in-htn", no_argument,       NULL,   'D'},
-		{"compile-goal"							  , no_argument,       NULL,   'g'},
-		
-		{"shop"                                   , no_argument,       NULL,   'S'},
-		{"shop2"                                  , no_argument,       NULL,   'S'},
-		{"shop1"                                  , no_argument,       NULL,   '1'},
-		{"hpdl"                                   , no_argument,       NULL,   'H'},
-		{"hddl"                                   , no_argument,       NULL,   'h'},
-		{"htn2strips"                             , no_argument,       NULL,   'R'}, // R because it is Ron alford's format
-		{"processed-hddl"                         , no_argument,       NULL,   'P'},
-		{"hddl-internal"                          , no_argument,       NULL,   'i'},
-		
-		{"panda-converter"                        , no_argument,       NULL,   'c'},
-		{"verify"                                 , optional_argument, NULL,   'v'},
-		{"vverify"                                , no_argument,       NULL,   'V'},
-		{"vvverify"                               , no_argument,       NULL,   'W'},
-		{"lenient"                                , no_argument,       NULL,   'l'},
-		{"verify-no-order"                        , no_argument,       NULL,   'o'},
-		
-		{"no-color"                               , no_argument,       NULL,   'C'},
-		{"debug"                                  , optional_argument, NULL,   'd'},
-		
-		{"properties"                             , optional_argument, NULL,   'p'},
-		
-		{NULL                                     , 0,                 NULL,   0},
-	};
+	gengetopt_args_info args_info;
+	if (cmdline_parser(argc, argv, &args_info) != 0) return 1;
 
-	bool optionsValid = true;
-	while (true) {
-		int c = getopt_long_only (argc, argv, "sS1HcvVWoCdkhilpLDgPR", options, NULL);
-		if (c == -1)
-			break;
-		if (c == '?' || c == ':'){
-			// Invalid option; getopt_long () will print an error message
-			optionsValid = false;
-			continue;
-		}
-
-		if (c == 's') splitParameters = false;
-		else if (c == 'k') compileConditionalEffects = false;
-		else if (c == 'L') { compileConditionalEffects = false; linearConditionalEffectExpansion = true; }
-		else if (c == 'D') encodeDisjunctivePreconditionsInMethods = true;
-		else if (c == 'g') compileGoalIntoAction = true;
-		else if (c == 'S') shopOutput = true;
-		else if (c == '1') { shopOutput = true; shop_1_compatability_mode = true; }
-	   	else if (c == 'H') hpdlOutput = true;
-	   	else if (c == 'h') pureHddlOutput = true;
-	   	else if (c == 'P') hddlOutput = true;
-	   	else if (c == 'R') htn2stripsOutput = true;
-	   	else if (c == 'i') { hddlOutput = true; internalHDDLOutput = true; }
-		else if (c == 'c') convertPlan = true;
-		else if (c == 'v') {
-			verifyPlan = true;
-			if (optarg) verbosity = atoi(optarg);
-		} else if (c == 'V') { verifyPlan = true; verbosity = 1; }
-		else if (c == 'W') { verifyPlan = true; verbosity = 2; }
-		else if (c == 'o') { verifyPlan = true; useOrderInPlanVerification = false; }
-		else if (c == 'l') { verifyPlan = true; lenientVerify = true; }
-		else if (c == 'C') no_colors_in_output = true;
-		else if (c == 'p') showProperties = true;
-		else if (c == 'd') {
-		   	verboseOutput = true;
-			if (optarg) verbosity = atoi(optarg);
-		}
+	// set debug mode
+	if (args_info.debug_given) {
+		verboseOutput = true;
+		verbosity = args_info.debug_arg;
 	}
-	if (!optionsValid) {
-		cout << "Invalid options. Exiting." << endl;
-		return 1;
+	if (args_info.no_colour_given) no_colors_in_output = true;
+	if (args_info.no_split_parameters_given) splitParameters = false;
+	if (args_info.keep_conditional_effects_given) compileConditionalEffects = false;
+	if (args_info.linear_conditional_effect_given) {
+		compileConditionalEffects = false; linearConditionalEffectExpansion = true;
+	}
+	if (args_info.encode_disjunctive_preconditions_in_htn_given) encodeDisjunctivePreconditionsInMethods = true;
+	if (args_info.goal_action_given) compileGoalIntoAction = true;
+
+	if (args_info.shop_given) shopOutput = true;
+	if (args_info.shop1_given) shopOutput = shop_1_compatability_mode = true;
+	if (args_info.hpdl_given) hpdlOutput = true;
+	if (args_info.hppdl_given) htn2stripsOutput = true;
+	if (args_info.hddl_given) pureHddlOutput = true;
+	if (args_info.processed_hddl_given) hddlOutput = true;
+	if (args_info.internal_hddl_given) hddlOutput = internalHDDLOutput = true;
+
+	if (args_info.verify_given){
+		verifyPlan = true;
+		verbosity = args_info.verify_arg;
+	}
+	if (args_info.vverify_given){
+		verifyPlan = true;
+		verbosity = 1;
+	}
+	if (args_info.vvverify_given){
+		verifyPlan = true;
+		verbosity = 2;
+	}
+	if (args_info.lenient_given) verifyPlan = lenientVerify = true;
+	if (args_info.verify_no_order_given) {
+		verifyPlan = true;
+		useOrderInPlanVerification = false;
 	}
 
-	for (int i = optind; i < argc; i++) {
-		if (dfile == -1) dfile = i;
-		else if (pfile == -1) pfile = i;
-		else if (doutfile == -1) doutfile = i;
-		else if (poutfile == -1) poutfile = i;
+	if (args_info.panda_converter_given) convertPlan = true;
+	if (args_info.properties_given) showProperties = true;
+
+	
+	cout << "pandaPIparser is configured as follows" << endl;
+	cout << "  Colurs in output: " << boolalpha << !no_colors_in_output << endl;
+	if (showProperties){
+		cout << "  Mode: show instance properties" << endl;
+	} else if (convertPlan){
+		cout << "  Mode: convert pandaPI plan" << endl;
+	} else if (verifyPlan){
+		cout << "  Mode: plan verification" << endl;
+		cout << "  Verbosity: " << verbosity << endl;
+		cout << "  Lenient mode: " << boolalpha << lenientVerify << endl;
+		cout << "  Ignore given order: " << !useOrderInPlanVerification << endl;
+	} else {
+		cout << "  Mode: parsing mode" << endl;
+		cout << "  Parameter splitting: " << boolalpha << splitParameters << endl;
+		cout << "  Conditional effects: ";
+		if (compileConditionalEffects){
+			if (linearConditionalEffectExpansion) cout << "linear encoding";
+			else cout << "exponential encoding";
+		} else cout << "keep";
+		cout << endl;	
+		cout << "  Disjunctive preconditions as HTN: " << boolalpha << encodeDisjunctivePreconditionsInMethods << endl;
+		cout << "  Replace goal with action: " << boolalpha << compileGoalIntoAction << endl;
+	
+		cout << "  Output: ";
+		if (shopOutput) cout << "SHOP2";
+		else if (shop_1_compatability_mode) cout << "SHOP1";
+		else if (hpdlOutput) cout << "HPDL";
+		else if (htn2stripsOutput) cout << "HPPDL";
+		else if (pureHddlOutput) cout << "HDDL (no transformations)";
+		else if (hddlOutput && internalHDDLOutput) cout << "HDDL (internal)";
+		else if (hddlOutput && !internalHDDLOutput) cout << "HDDL (with transformations)";
+		else cout << "pandaPI format";
+		cout << endl;
 	}
+
+
+	vector<string> inputFiles;
+	for (unsigned i = 0 ; i < args_info.inputs_num; i++)
+    	inputFiles.push_back(args_info.inputs[i]);
+
+	if (inputFiles.size() > 0) dfile = 0;
+	if (inputFiles.size() > 1) pfile = 1;
+	if (inputFiles.size() > 2) pfile = 2;
+	if (inputFiles.size() > 3) pfile = 3;
 
 	if (dfile == -1){
 		if (convertPlan)
@@ -167,12 +180,12 @@ int main(int argc, char** argv) {
 
 	// if we want to simplify a plan, just parse nothing
 	if (convertPlan){
-		ifstream * plan   = new ifstream(argv[dfile]);
+		ifstream * plan   = new ifstream(inputFiles[dfile]);
 		ostream * outplan = &cout;
 		if (pfile != -1){
-			ofstream * of  = new ofstream(argv[pfile]);
+			ofstream * of  = new ofstream(inputFiles[pfile]);
 			if (!of->is_open()){
-				cout << "I can't open " << argv[pfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[pfile] << "!" << endl;
 				return 2;
 			}
 			outplan = of;
@@ -189,27 +202,27 @@ int main(int argc, char** argv) {
 	}
 
 	// open c-style file handle 
-	FILE *domain_file = fopen(argv[dfile], "r");
-	FILE *problem_file = fopen(argv[pfile], "r");
+	FILE *domain_file = fopen(inputFiles[dfile].c_str(), "r");
+	FILE *problem_file = fopen(inputFiles[pfile].c_str(), "r");
 
 	if (!domain_file) {
-		cout << "I can't open " << argv[dfile] << "!" << endl;
+		cout << "I can't open " << inputFiles[dfile] << "!" << endl;
 		return 2;
 	}
 	if (!problem_file) {
-		cout << "I can't open " << argv[pfile] << "!" << endl;
+		cout << "I can't open " << inputFiles[pfile] << "!" << endl;
 		return 2;
 	}
 	if (!shopOutput && !hpdlOutput && !hddlOutput && !pureHddlOutput && !htn2stripsOutput && poutfile != -1){
-		cout << "For ordinary pandaPI output, you may only specify one output file, but you specified two: " << argv[doutfile] << " and " << argv[poutfile] << endl;
+		cout << "For ordinary pandaPI output, you may only specify one output file, but you specified two: " << inputFiles[doutfile] << " and " << inputFiles[poutfile] << endl;
 	}
 	
 	// parsing of command line arguments has been completed	
 		
 		
 	// parse the domain file
-	run_parser_on_file(domain_file, argv[dfile]);
-	run_parser_on_file(problem_file, argv[pfile]);
+	run_parser_on_file(domain_file, (char*) inputFiles[dfile].c_str());
+	run_parser_on_file(problem_file, (char*) inputFiles[pfile].c_str());
 
 	if (showProperties){
 		printProperties();
@@ -221,17 +234,17 @@ int main(int argc, char** argv) {
 		ostream * dout = &cout;
 		ostream * pout = &cout;
 		if (doutfile != -1){
-			ofstream * df  = new ofstream(argv[doutfile]);
+			ofstream * df  = new ofstream(inputFiles[doutfile]);
 			if (!df->is_open()){
-				cout << "I can't open " << argv[doutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[doutfile] << "!" << endl;
 				return 2;
 			}
 			dout = df;
 		}
 		if (poutfile != -1){
-			ofstream * pf  = new ofstream(argv[poutfile]);
+			ofstream * pf  = new ofstream(inputFiles[poutfile]);
 			if (!pf->is_open()){
-				cout << "I can't open " << argv[poutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[poutfile] << "!" << endl;
 				return 2;
 			}
 			pout = pf;
@@ -254,7 +267,7 @@ int main(int argc, char** argv) {
 
 	// do not preprocess the instance at all if we are validating a solution
 	if (verifyPlan){
-		ifstream * plan  = new ifstream(argv[doutfile]);
+		ifstream * plan  = new ifstream(inputFiles[doutfile]);
 		bool result = verify_plan(*plan, useOrderInPlanVerification, lenientVerify, verbosity);
 		cout << "Plan verification result: ";
 		if (result) cout << color(COLOR_GREEN,"true",MODE_BOLD);
@@ -277,17 +290,17 @@ int main(int argc, char** argv) {
 		ostream * dout = &cout;
 		ostream * pout = &cout;
 		if (doutfile != -1){
-			ofstream * df  = new ofstream(argv[doutfile]);
+			ofstream * df  = new ofstream(inputFiles[doutfile]);
 			if (!df->is_open()){
-				cout << "I can't open " << argv[doutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[doutfile] << "!" << endl;
 				return 2;
 			}
 			dout = df;
 		}
 		if (poutfile != -1){
-			ofstream * pf  = new ofstream(argv[poutfile]);
+			ofstream * pf  = new ofstream(inputFiles[poutfile]);
 			if (!pf->is_open()){
-				cout << "I can't open " << argv[poutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[poutfile] << "!" << endl;
 				return 2;
 			}
 			pout = pf;
@@ -314,17 +327,17 @@ int main(int argc, char** argv) {
 		ostream * dout = &cout;
 		ostream * pout = &cout;
 		if (doutfile != -1){
-			ofstream * df  = new ofstream(argv[doutfile]);
+			ofstream * df  = new ofstream(inputFiles[doutfile]);
 			if (!df->is_open()){
-				cout << "I can't open " << argv[doutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[doutfile] << "!" << endl;
 				return 2;
 			}
 			dout = df;
 		}
 		if (poutfile != -1){
-			ofstream * pf  = new ofstream(argv[poutfile]);
+			ofstream * pf  = new ofstream(inputFiles[poutfile]);
 			if (!pf->is_open()){
-				cout << "I can't open " << argv[poutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[poutfile] << "!" << endl;
 				return 2;
 			}
 			pout = pf;
@@ -333,9 +346,9 @@ int main(int argc, char** argv) {
 	} else {
 		ostream * dout = &cout;
 		if (doutfile != -1){
-			ofstream * df  = new ofstream(argv[doutfile]);
+			ofstream * df  = new ofstream(inputFiles[doutfile]);
 			if (!df->is_open()){
-				cout << "I can't open " << argv[doutfile] << "!" << endl;
+				cout << "I can't open " << inputFiles[doutfile] << "!" << endl;
 				return 2;
 			}
 			dout = df;
